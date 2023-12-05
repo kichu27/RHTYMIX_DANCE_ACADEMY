143201174cfd639f25e7152a9fee7b30aa9e0497
Kartik Patekar <patekar.kartik@coea.ac.in>
2023-12-06 01:44:42 +0530

    223

--- src/pages/api/getblogdata.js
+++ src/pages/api/getblogdata.js
@@ -1,6 +1,47 @@
-import mysql from "mysql2/promise";
+// import mysql from "mysql2/promise";
 
-export default async function handler(req, res) {
+// export default async function handler(req, res) {
+//   try {
+//     // Create a connection pool
+//     const dbPool = await mysql.createPool({
+//       host: 'localhost',
+//       user: 'root',
+//       database: 'db',
+//       waitForConnections: true,
+//       connectionLimit: 10, // Adjust the limit as needed
+//       queueLimit: 0
+//     });
+
+//     // Acquire a connection from the pool
+//     const dbConnection = await dbPool.getConnection();
+
+//     const query = "SELECT * FROM blogdata";
+//     const [data] = await dbConnection.execute(query);
+    
+//     // Release the acquired connection back to the pool
+//     dbConnection.release();
+
+//     res.status(200).json({ results: data });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// }
+
+
+import mysql from "mysql2/promise";
+import express from "express";
+
+const app = express();
+
+// Middleware to enable CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://rhtymix-dance-academy.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.get("/api/getblogdata", async (req, res) => {
   try {
     // Create a connection pool
     const dbPool = await mysql.createPool({
@@ -17,7 +58,7 @@ export default async function handler(req, res) {
 
     const query = "SELECT * FROM blogdata";
     const [data] = await dbConnection.execute(query);
-    
+
     // Release the acquired connection back to the pool
     dbConnection.release();
 
@@ -25,4 +66,9 @@ export default async function handler(req, res) {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
